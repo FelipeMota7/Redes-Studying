789624debb7983338772a99ad449b62b0f8fe0d1
lui <luixp@kali.luizxp>
2022-10-29 12:00:56 -0300

    ssh-client

--- ssh_client.py
+++ ssh_client.py
@@ -1,1 +1,14 @@
 import paramiko
+
+host = "0.0.0.0"
+user = ""
+passwd = ""
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(host, username=user, password=passwd)
+
+while True:
+    stdin, stdout, stderr = client.exec_command(input("Comando: "))
+    for line in stdout.readlines():
+        print(line.strip())
