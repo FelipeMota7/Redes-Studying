679c337b9f161acba62fa3413cdf859a4c1d6028
lui <luixp@kali.luizxp>
2022-11-01 11:07:52 -0300

    scanning

---
+++ port_scanning.py
@@ -0,0 +1,12 @@
+import socket
+
+host = "mixpix.com.br"
+ports = [21, 22, 23, 25, 80, 443, 445, 8080, 844, 3306, 139, 135]
+
+for port in ports:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(0.05)
+    code = client.connect_ex((host, port))
+    if code == 0:
+        print("[+] {} open".format(port))
+    
