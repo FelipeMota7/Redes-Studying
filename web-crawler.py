743470a6ee2834829169d09ec29cdc00d581d03b
lui <luixp@kali.luizxp>
2022-11-06 18:48:09 -0300

    crawlers

--- web-crawler.py
+++ web-crawler.py
@@ -8,7 +8,7 @@ CRAWLED = set()
 
 
 def request(url):
-    header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"}
+    header = {""}
     try:
         response = requests.get(url, headers=header)
         return response.text
